f64dc604ac0074940611212542e8dfe9d192f1af
sbaitipro <abdelkhalek.sbaiti.pro@gmail.com>
2025-06-04 03:17:36 +0100

    updated

--- database/sql/python.codes.sql
+++ database/sql/python.codes.sql
@@ -1,7 +1,7 @@
 INSERT OR IGNORE INTO quizzes(id, user_id, title, description,status) 
 VALUES (4 , 6038394083, "Python codes", "Simple python codes questions for starter!","published");
 
-DELETE FROM question
+DELETE FROM questions;
 
 INSERT INTO questions(quiz_id, media_url, question, options, status, explanation)
 VALUES
