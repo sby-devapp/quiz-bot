e88060e51e275197022ee24e109c10fa42825bde
sbaitipro <abdelkhalek.sbaiti.pro@gmail.com>
2025-06-06 16:51:54 +0100

    updated

--- database/sql/javascript.codes.sql
+++ database/sql/javascript.codes.sql
@@ -5,35 +5,163 @@ DELETE FROM questions WHERE quiz_id = 5;
 
 INSERT INTO questions(quiz_id, media_url, question, options, status, explanation) VALUES
 (5, NULL,
-'*What will this code output?*\n```JavaScript\n
-console.log(typeof undefined)\n```', 'undefined|null|string|object', 'published', 'typeof undefined is "undefined"'),
-(5, NULL, '*What will this code output?*\n```JavaScript\nconsole.log(10 / 2)\n```', '5|2|0|Error', 'published', '10 divided by 2 is 5'),
-(5, NULL, '*What will this code output?*\n```JavaScript\nconsole.log("5" * 2)\n```', '10|52|NaN|Error', 'published', 'String "5" is coerced to number'),
-(5, NULL, '*What will this code output?*\n```JavaScript\nconsole.log([].push(1))\n```', '1|0|undefined|Error', 'published', 'push returns new length of array'),
-(5, NULL, '*What will this code output?*\n```JavaScript\nconsole.log(Math.max(1, 5, 3))\n```', '5|3|1|undefined', 'published', 'Math.max returns largest value'),
-(5, NULL, '*What will this code output?*\n```JavaScript\nconsole.log(Boolean(0))\n```', 'false|true|undefined|Error', 'published', '0 is falsy'),
-(5, NULL, '*What will this code output?*\n```JavaScript\nconsole.log("abc".toUpperCase())\n```', 'ABC|abc|Abc|undefined', 'published', 'toUpperCase returns uppercase string'),
-(5, NULL, '*What will this code output?*\n```JavaScript\nconsole.log([1,2,3].length)\n```', '3|2|1|undefined', 'published', 'Array length is 3'),
-(5, NULL, '*What will this code output?*\n```JavaScript\nconsole.log(typeof NaN)\n```', 'number|NaN|undefined|string', 'published', 'typeof NaN is "number"'),
-(5, NULL, '*What will this code output?*\n```JavaScript\nconsole.log(2 > 1)\n```', 'true|false|undefined|Error', 'published', '2 is greater than 1'),
-(5, NULL, '*What will this code output?*\n```JavaScript\nconsole.log("a" + "b")\n```', 'ab|a b|a|b', 'published', 'String concatenation'),
-(5, NULL, '*What will this code output?*\n```JavaScript\nconsole.log([1,2,3].reverse()[0])\n```', '3|1|2|undefined', 'published', 'reverse reverses array'),
-(5, NULL, '*What will this code output?*\n```JavaScript\nconsole.log(typeof function(){})\n```', 'function|object|undefined|string', 'published', 'typeof function is "function"'),
-(5, NULL, '*What will this code output?*\n```JavaScript\nconsole.log(4 % 2)\n```', '0|2|1|undefined', 'published', '4 modulo 2 is 0'),
-(5, NULL, '*What will this code output?*\n```JavaScript\nconsole.log("5" - 2)\n```', '3|7|NaN|Error', 'published', 'String "5" is coerced to number'),
-(5, NULL, '*What will this code output?*\n```JavaScript\nconsole.log([1,2,3].includes(2))\n```', 'true|false|undefined|Error', 'published', 'includes checks if array contains value'),
-(5, NULL, '*What will this code output?*\n```JavaScript\nconsole.log("abc".indexOf("b"))\n```', '1|0|2|-1', 'published', 'indexOf returns index of substring'),
-(5, NULL, '*What will this code output?*\n```JavaScript\nconsole.log(typeof [])\n```', 'object|array|undefined|string', 'published', 'typeof array is "object"'),
-(5, NULL, '*What will this code output?*\n```JavaScript\nconsole.log(1 + true)\n```', '2|1|true|Error', 'published', 'true is coerced to 1'),
-(5, NULL, '*What will this code output?*\n```JavaScript\nconsole.log("10" / "2")\n```', '5|2|NaN|Error', 'published', 'Strings are coerced to numbers'),
-(5, NULL, '*What will this code output?*\n```JavaScript\nconsole.log(typeof "")\n```', 'string|object|undefined|boolean', 'published', 'typeof empty string is "string"'),
-(5, NULL, '*What will this code output?*\n```JavaScript\nconsole.log([1,2,3].shift())\n```', '1|2|3|undefined', 'published', 'shift removes and returns first element'),
-(5, NULL, '*What will this code output?*\n```JavaScript\nconsole.log("abc".replace("b", "d"))\n```', 'adc|abc|dbc|abd', 'published', 'replace replaces first occurrence'),
-(5, NULL, '*What will this code output?*\n```JavaScript\nconsole.log(2 < 1)\n```', 'false|true|undefined|Error', 'published', '2 is not less than 1'),
+'*What will this code output?*
+```javascript
+console.log(typeof undefined)
+```', 
+'undefined|null|string|object', 
+'published', 
+'typeof undefined is "undefined"'),
+
+(5, NULL, 
+'*What will this code output?*
+```javascript
+console.log(10 / 2)
+```', 
+'5|2|0|Error', 'published', '10 divided by 2 is 5'),
+(5, NULL, 
+'*What will this code output?
+```javascript
+var str = "5" * 2
+console.log(str)
+```',
+ '10|52|NaN|Error', 'published', 'String "5" is coerced to number'),
+(5, NULL, 
+'*What will this code output?*
+```javascript
+console.log([].push(1))
+```', 
+'1|0|undefined|Error', 'published', 'push returns new length of array'),
+(5, NULL, 
+'*What will this code output?*
+```javascript
+console.log(Math.max(1, 5, 3))
+```', '5|3|1|undefined', 'published', 'Math.max returns largest value'),
+(5, NULL, 
+'*What will this code output?*
+```javascript
+console.log(Boolean(0))
+```', 'false|true|undefined|Error', 'published', '0 is falsy'),
+(5, NULL, 
+'*What will this code output?*
+```javascript
+var str = "abc".toUpperCase()
+console.log(str)
+```', 
+'ABC|abc|Abc|undefined', 'published', 'toUpperCase returns uppercase string'),
+(5, NULL, 
+'*What will this code output?*
+```javascript
+var li = [1,2,3]
+console.log(li.length)
+```', 
+'3|2|1|undefined', 'published', 'Array length is 3'),
+(5, NULL, 
+'*What will this code output?*
+```javascript
+console.log(typeof NaN)
+```', 
+'number|NaN|undefined|string', 'published', 'typeof NaN is "number"'),
+(5, NULL, 
+'*What will this code output?*
+```javascript
+console.log(2 > 1)
+```', 
+'true|false|undefined|Error', 'published', '2 is greater than 1'),
+(5, NULL, 
+'*What will this code output?*
+```javascript
+console.log("a" + "b")
+```', 
+'ab|a b|a|b', 'published', 'String concatenation'),
+(5, NULL, 
+'*What will this code output?*
+```javascript
+li = [1,2,3].reverse()[0]
+console.log(li)
+```', 
+'3|1|2|undefined', 'published', 'reverse reverses array'),
+(5, NULL, 
+'*What will this code output?*
+```javascript
+console.log(typeof function(){})
+```', 
+'function|object|undefined|string', 'published', 'typeof function is "function"'),
+(5, NULL, 
+'*What will this code output?*
+```javascript
+console.log(4 % 2)
+```', 
+'0|2|1|undefined', 'published', '4 modulo 2 is 0'),
+(5, NULL, 
+'*What will this code output?*
+```javascript
+console.log("5" - 2)
+```', 
+'3|7|NaN|Error', 'published', 'String "5" is coerced to number'),
+(5, NULL, 
+'*What will this code output?*
+```javascript
+var li = [1,2,3]
+console.log(li.includes(2))
+```', 
+'true|false|undefined|Error', 'published', 'includes checks if array contains value'),
+(5, NULL, 
+'*What will this code output?*
+```javascript
+var str = "abc"
+console.log(str.indexOf("b"))
+```', 
+'1|0|2|-1', 'published', 'indexOf returns index of substring'),
+(5, NULL, 
+'*What will this code output?*
+```javascript
+console.log(typeof [])
+```', 
+'object|array|undefined|string', 'published', 'typeof array is "object"'),
+(5, NULL, 
+'*What will this code output?*
+```javascript
+console.log(1 + true)
+```', 
+'2|1|true|Error', 'published', 'true is coerced to 1'),
+(5, NULL, 
+'*What will this code output?
+```javascript
+console.log("10" / "2")
+```', 
+'5|2|NaN|Error', 'published', 'Strings are coerced to numbers'),
+(5, NULL, 
+'*What will this code output?*
+```javascript
+console.log(typeof "")
+```', 
+'string|object|undefined|boolean', 'published', 'typeof empty string is "string"'),
+(5, NULL, 
+'*What will this code output?*
+```javascript
+var li1 = [1,2,3]
+var li2 = li1.shift()
+console.log(li2)
+```', 
+'1|2|3|undefined', 'published', 'shift removes and returns first element'),
+(5, NULL, 
+'*What will this code output?*
+```javascript
+var txt = "abc"
+console.log(txt.replace("b", "d"))
+```', 
+'adc|abc|dbc|abd', 'published', 'replace replaces first occurrence'),
+(5, NULL, 
+'*What will this code output?*
+```javascript
+console.log(2 < 1)
+```', 
+'false|true|undefined|Error', 'published', '2 is not less than 1'),
 (5,
 NULL, 
  '*What will this code output?*
- ```JavaScript
+ ```javascript
  console.log(typeof null === "object")
  ```', 
  'true|false|undefined|Error', 
@@ -42,7 +170,7 @@ NULL,
  (5,
  NULL, 
  '*What will this code output?*
- ```JavaScript
+ ```javascript
  var fan = function(text){
     console.log(text)
  };
